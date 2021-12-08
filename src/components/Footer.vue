4ba87b3c068b95edd878b7c7c80cb367cbccaf32
s9921316 <audiowalk.art@gmail.com>
2021-12-09 05:08:57 +0800

    1208
    Description ..

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="footer">
-        <div class="container h-100">
-            <div class="row p-5 h-100 justify-content-center align-items-center">
-            <!--footer畫面左區-->
-            <div class="col-sm-12 col-md-7">
-                <h3>各校資源</h3>
+        <div class="h-auto">
+            <div class="row h-auto justify-content-center align-items-center">
+           
+            <!--footer畫面右區-->
+            <div class="col-sm-12 col-md-8 m-2">
+                <img src="https://firebasestorage.googleapis.com/v0/b/nm-tayal.appspot.com/o/PDF%2Ffirefish.jpeg?alt=media&token=af06762c-aa4e-433d-8c81-5d6058afc102" alt="" width="1300">
+            </div>
+
+             <!--footer畫面左區-->
+            <div class="col-sm-12 col-md-2">
+                <h3>盤點各校資源</h3>
                 <p>新竹尖石地區的 泰雅核心素養，逐漸成形，1L.2S.3T 將是我們重要的願景</p>
-                <button @click="gostores" class="btn custom-btn btn-outline-primary mr-4">
+                <button @click="gostores" class="btn custom-btn btn-outline-primary">
                   瞭解更多
                 </button>
             </div>
-            <!--footer畫面右區-->
-            <div class="col-sm-12 col-md-4 m-3">
-                <img src="/img/layout/footer.jpg" alt="" width="300">
-            </div>
+
             </div>
         </div>
     </div>
@@ -35,9 +38,9 @@ export default {
  
 <style scoped lang="scss">
   .footer{
-    padding-top: 7rem;
+    // padding-top: 3rem;
     width: 100%;
-    min-height: 500px;
+    // min-height: 500px;
     text-align: left;
     color: #ffffff;
     background-color:darkslategray;
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="header">
-        <div class="container h-100">
-          <div class="row h-100 justify-content-center align-items-center">
+        <div class="container h-auto">
+          <div class="row h-auto justify-content-center align-items-center">
             <!--header畫面左區-->
-            <div class="col-sm-12 col-md-6">
+            <div class="col-sm-12 col-md-3">
               <div class="header-content">
                 <h1 class="header-title">
                   教案查詢網站
@@ -20,9 +20,10 @@
               </div>
             </div>
             <!--header畫面右區-->
-            <div class="col-sm-12 col-md-5 m-3">
+            <div class="col-sm-12 col-md-8 mt-3">
                 <div class="graphic">
-                    <img src="/img/layout/header.jpg" alt="" width="400">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/nm-tayal.appspot.com/o/PDF%2F20191126%E5%9B%9B%E5%B9%B4%E7%B4%9A%E5%8C%97%E5%BE%97%E6%8B%89%E6%9B%BC%E9%81%8A%E8%A8%98_200809_3-1140x570.jpeg?alt=media&token=86be97de-e950-4b0f-8a93-d6570809e899" 
+                         alt="" width="1200">
                 </div>
             </div>
           </div>
@@ -49,10 +50,10 @@ export default {
  
 <style scoped lang="scss">
   .header{
-    padding-top: 7rem;
+    
     width: 100%;
-    min-height: 500px;
+    // min-height: 500px;
     text-align: left;
-    background-color:rgb(56, 115, 233);
+    background-color:rgb(46, 153, 163);
   }
 </style>
